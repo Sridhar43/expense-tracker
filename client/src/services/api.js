3590c4d71556be0366a527251953242a003a6fd6
Sridhar43 <panigrahisridhar464@gmail.com>
2026-07-30 20:17:47 +0530

    kkk

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expense-tracker-zay5.onrender.com/api",
+  baseURL: "https://expense-tracker-zay5.onrender.com",
 });
 
 export default API;
